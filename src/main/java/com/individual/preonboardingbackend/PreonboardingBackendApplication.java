b69793472dabef2491364ef31e01bda33ef20fab
hyeonseok <ghostpar12@naver.com>
2024-08-10 00:13:05 +0900

    [Feat] JPA, MySQL 종속성 추가 및 jWT 시크릿키 추가

---
+++ src/main/java/com/individual/preonboardingbackend/PreonboardingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.individual.preonboardingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreonboardingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreonboardingBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/individual/preonboardingbackend/PreonboardingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.individual.preonboardingbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreonboardingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
