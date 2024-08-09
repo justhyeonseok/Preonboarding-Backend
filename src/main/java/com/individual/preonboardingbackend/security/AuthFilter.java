91c64e1aa101a07897bec9dacccd4a1783e842fc
hyeonseok <ghostpar12@naver.com>
2024-08-10 04:26:42 +0900

    [Feat] 추가적인 로그인을 보완할 filter 구현체를 직접 작성

---
+++ src/main/java/com/individual/preonboardingbackend/security/AuthFilter.java
@@ -0,0 +1,70 @@
+package com.individual.preonboardingbackend.security;
+
+import com.individual.preonboardingbackend.user.model.User;
+import com.individual.preonboardingbackend.user.repository.UserRepository;
+import io.jsonwebtoken.Claims;
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import java.io.IOException;
+@Slf4j
+@Component
+@Order(2)
+public class AuthFilter implements Filter {
+    private final UserRepository userRepository;
+    private final JwtUtil jwtUtil;
+
+    public AuthFilter(UserRepository userRepository, JwtUtil jwtUtil) {
+        this.userRepository = userRepository;
+        this.jwtUtil = jwtUtil;
+    }
+
+    @Override
+    public void doFilter(
+            ServletRequest servletRequest,
+            ServletResponse servletResponse,
+            FilterChain filterChain) throws IOException,
+            ServletException {
+
+        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+        String url = httpServletRequest.getRequestURI();
+
+        if (StringUtils.hasText(url) &&
+                (url.startsWith("/api/v1/user")) // 인증 제외 url
+        ) {
+            log.info("인증처리를 하지 않는 URL{}", url);
+            filterChain.doFilter(servletRequest, servletResponse);
+
+            // 토큰 확인
+            String tokenValue = jwtUtil.getTokenFromRequest(httpServletRequest);
+
+            if (StringUtils.hasText(tokenValue)) {
+                // JWT 토큰 substring
+                String token = jwtUtil.substringToken(tokenValue);
+
+                // 토큰 검증
+                if (!jwtUtil.validateToken(token)) {
+                    throw new IllegalArgumentException("Token Error");
+                }
+
+                // 토큰에서 사용자 정보 가져오기
+                Claims info = jwtUtil.getUserInfoFromToken(token);
+
+                User user = userRepository.findByUsername(info.getSubject()).orElseThrow(() ->
+                        new NullPointerException("Not Found User")
+                );
+
+                servletRequest.setAttribute("user", user);
+                filterChain.doFilter(servletRequest, servletResponse); // 다음 Filter 로 이동
+            } else {
+                throw new IllegalArgumentException("Not Found Token");
+            }
+        }
+
+    }
+}
+
---
+++ src/main/java/com/individual/preonboardingbackend/security/LoggingFilter.java
@@ -0,0 +1,25 @@
+package com.individual.preonboardingbackend.security;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import java.io.IOException;
+@Slf4j(topic = "loggingFilter")
+@Component
+@Order(1)
+public class LoggingFilter implements Filter {
+    @Override
+    public void doFilter(
+            ServletRequest servletRequest,
+            ServletResponse servletResponse,
+            FilterChain filterChain
+    ) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        String url = request.getRequestURI();
+        log.info(url);
+        filterChain.doFilter(servletRequest, servletResponse);
+        log.info("Filter completed");
+    }
+}
