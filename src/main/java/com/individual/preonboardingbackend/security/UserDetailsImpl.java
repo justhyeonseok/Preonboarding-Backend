bad8b0c94e0ea2b2da18bb169f32cb32ec90c3f0
hyeonseok <ghostpar12@naver.com>
2024-08-11 02:09:45 +0900

    [Feat] UserDetails 구현체 작성

---
+++ src/main/java/com/individual/preonboardingbackend/security/UserDetailsImpl.java
@@ -0,0 +1,52 @@
+package com.individual.preonboardingbackend.security;
+
+import com.individual.preonboardingbackend.user.model.User;
+import com.individual.preonboardingbackend.user.model.UserRole;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public record UserDetailsImpl(User user) implements UserDetails {
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        UserRole role = user.getRole();
+        String authority = role.getAuthority();
+        SimpleGrantedAuthority simpleGrantedAuthority = new SimpleGrantedAuthority(authority);
+        Collection<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(simpleGrantedAuthority);
+        return authorities;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
